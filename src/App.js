876410b53438d82a108a776e1366ccba68fc4612
AleksTheDev <74507022+AleksTheDev@users.noreply.github.com>
2021-07-06 00:22:03 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,10 +12,8 @@ function App() {
         <a
           className="App-link"
           href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+          Learn React!
         </a>
       </header>
     </div>
