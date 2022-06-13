dea71c50114f3744e1bef87106bb228f00f8d11d
Alexander Angelov <aleksanderangburgas6@gmail.com>
2022-06-13 19:41:55 +0300

    Revert "add progressier manifest and script"
    This reverts commit 992f45ef7c0a92d4193bb6d7fa5c2685ab3d7d09.

--- app.vue
+++ app.vue
@@ -1,25 +1,17 @@
 <script setup>
-useHead({
-  title: 'Nitro Generator V2',
-  link: {
-    rel: 'manifest',
-    href: 'https://progressier.com/client/progressier.json?id=FIttVyfmA2GYn7j0MDxv'
-  },
-  script: {
-    src: 'https://progressier.com/client/script.js?id=FIttVyfmA2GYn7j0MDxv',
-    defer: true
-  },
-  charset: 'utf-8',
-  meta: [
-    {
-      name: 'description',
-      content: 'A free Discord nitro generator'
+  useHead({
+    title: 'Nitro Generator V2',
+    charset: 'utf-8',
+    meta: [
+      {
+        name: 'description',
+        content: 'A free Discord nitro generator'
+      }
+    ],
+    htmlAttrs: {
+      lang: 'en'
     }
-  ],
-  htmlAttrs: {
-    lang: 'en'
-  }
-})
+  })
 </script>
 
 <template>
@@ -29,14 +21,14 @@ useHead({
 </template>
 
 <style>
-html {
-  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-  background-color: #282c34;
-  text-align: center;
-  min-height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
+  html {
+    font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;
+    background-color: #282c34;
+    text-align: center;
+    min-height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
