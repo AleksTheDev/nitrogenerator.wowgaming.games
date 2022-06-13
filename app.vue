992f45ef7c0a92d4193bb6d7fa5c2685ab3d7d09
Alexander Angelov <aleksanderangburgas6@gmail.com>
2022-06-13 19:01:21 +0300

    add progressier manifest and script

--- app.vue
+++ app.vue
@@ -1,17 +1,25 @@
 <script setup>
-  useHead({
-    title: 'Nitro Generator V2',
-    charset: 'utf-8',
-    meta: [
-      {
-        name: 'description',
-        content: 'A free Discord nitro generator'
-      }
-    ],
-    htmlAttrs: {
-      lang: 'en'
+useHead({
+  title: 'Nitro Generator V2',
+  link: {
+    rel: 'manifest',
+    href: 'https://progressier.com/client/progressier.json?id=FIttVyfmA2GYn7j0MDxv'
+  },
+  script: {
+    src: 'https://progressier.com/client/script.js?id=FIttVyfmA2GYn7j0MDxv',
+    defer: true
+  },
+  charset: 'utf-8',
+  meta: [
+    {
+      name: 'description',
+      content: 'A free Discord nitro generator'
     }
-  })
+  ],
+  htmlAttrs: {
+    lang: 'en'
+  }
+})
 </script>
 
 <template>
@@ -21,14 +29,14 @@
 </template>
 
 <style>
-  html {
-    font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;
-    background-color: #282c34;
-    text-align: center;
-    min-height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
+html {
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  background-color: #282c34;
+  text-align: center;
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
 </style>
